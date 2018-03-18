cc8a04dd9b9946ebf73c1c0f49c60fb63999ae98
BenjaminDagg <bdagg@csumb.edu>
2018-03-17 20:15:28 -0700

    updated map to only display parks, sport complexes, schools, roads

--- CST438-project/CST439-project/src/app/components/map/map.component.ts
+++ CST438-project/CST439-project/src/app/components/map/map.component.ts
@@ -10,14 +10,73 @@ export class MapComponent implements OnInit {
 
 
   title: string = 'My first AGM project';
-  lat: number = 52.678418;
-  lng: number = 7.809007;
+  lat: number = 36.6516;
+  lng: number = -121.7978;
+  mapType: string = 'roadmap'
+  map: {};
+  
 
-  constructor() { 
-  	
+  constructor() {
+  
+  	//building map objec to pass into map selector in html 
+  	this.map = {
+  		center: {
+  			lat: this.lat,
+  			lng: this.lng
+  		},
+  		zoom: 10,
+  		mapTypeId: this.mapType,
+  		styles: [
+  			
+			{
+				"featureType": "landscape",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.attraction",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.business",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.government",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.medical",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.park",
+				"stylers": [{"visibility":"on"}]
+			},
+			{
+				"featureType": "poi.place_of_worship",
+				"stylers": [{"visibility":"off"}]
+			},
+			{
+				"featureType": "poi.school",
+				"stylers": [{"visibility":"on"}]
+			},
+			{
+				"featureType": "poi.sports_complex",
+				"stylers": [{"visibility":"on"}]
+			},
+			{
+				"featureType": "transit",
+				"stylers": [{"visibility":"off"}]
+			}
+  		]
+  	};
   }
 
   ngOnInit() {
+  }
+  
+  click($event) {
+  	console.log($event.coords.lat + ' ' + $event.coords.lng);
   }
 
 }
