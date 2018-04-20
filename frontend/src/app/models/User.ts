96f290ec808d8378c918d7c0632e422d008be746
Benjamin Holland <ben.holland.core@gmail.com>
2018-04-20 13:12:33 -0700

    Added Interface and Concrete Model for Users

--- frontend/src/app/models/User.ts
+++ frontend/src/app/models/User.ts
@@ -1,8 +1,21 @@
 
+export interface User {
+  uid:string;
+  displayName:string;
+  email:string;
+  phoneNumber:string;
+  photoUrl:string;
+}
+export class UserModel implements User{
 
-export class User {
+  constructor(
+    public uid:string,
+    public displayName:string,
+    public email:string,
+    public phoneNumber:string,
+    public photoUrl:string){
 
-  _id : string;
-  displayName: string;
-  email: string;
+  }
+
+
 }
