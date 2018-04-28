71175c947627d22dbc773e3294f54919270f3646
Benjamin Holland <ben.holland.core@gmail.com>
2018-04-28 15:21:30 -0700

    Fixed chat service so server will run both the chat relay AND the web server

--- frontend/src/app/services/chat/chat.service.ts
+++ frontend/src/app/services/chat/chat.service.ts
@@ -3,22 +3,26 @@ import  * as io from 'socket.io-client';
 import { Observable } from 'rxjs/Observable';
 import { UserService } from '../user-service/user.service';
 
+export interface Message{
+  message: string;
+  username: string;
+}
 @Injectable()
 export class ChatService {
 
 
-  private socket:
+  private socket:SocketIOClient.Socket=null;
 
   constructor(private userService: UserService) {
     console.log('in constructor');
-    this.socket = io('http://localhost:3000');
+    this.socket = io.connect('http://localhost:3000');
 
 
 
   }
 
 
-  sendMessage(message) {
+  sendMessage(message:string) {
     var msg = {
       message: message,
       username: this.userService.getUser().displayName
@@ -30,7 +34,6 @@ export class ChatService {
   onMessage() {
     return Observable.create(observer => {
       this.socket.on('serverMessage', msg => {
-
         observer.next(msg);
       })
     })
